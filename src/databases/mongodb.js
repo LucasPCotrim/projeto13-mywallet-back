e20133899568a7214884d9ef256be25f1da266e1
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-08 20:43:54 -0300

    refactor: Refactored mongodb.js

--- src/databases/mongodb.js
+++ src/databases/mongodb.js
@@ -3,16 +3,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-let db = null;
-
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 try {
   await mongoClient.connect();
-  db = mongoClient.db(process.env.MONGO_DATABASE_NAME);
   console.log('Database connected successfully!');
 } catch (error) {
+  console.log(error);
   console.error('Error when connecting to Database...');
 }
+
+const db = mongoClient.db(process.env.MONGO_DATABASE_NAME);
 
 export default db;
