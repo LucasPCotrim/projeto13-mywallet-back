bf2f6c6618aa85caa546f4478b1862ba587613da
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-08 03:24:07 -0300

    feat: Implemented POST on "/login" route

--- src/schemas/authenticationSchemas.js
+++ src/schemas/authenticationSchemas.js
@@ -6,3 +6,8 @@ export const authSignUpSchema = joi.object({
   password: joi.string().required(),
   confirmPassword: joi.ref('password'),
 });
+
+export const authLoginSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+});
