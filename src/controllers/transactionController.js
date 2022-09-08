91c006164a845d98091844d50374ffa2e27bfc13
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-08 04:00:18 -0300

    feat: Initial implementation of GET from "/transactions" route

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,1 +1,27 @@
 import db from '../databases/mongodb.js';
+
+export async function getTransactions(req, res) {
+  try {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    if (!token) return res.status(401).send({ message: 'Error: Invalid authentication token!' });
+
+    const session = await db.collection('sessions').findOne({ token });
+    if (!session) {
+      return res.status(401).send({ message: 'Error: Session timeout! Please log in again' });
+    }
+
+    const user = await db.collection('users').findOne({ _id: session.userId });
+    if (!user) {
+      return res.status(404).send({ message: 'Error: User not found!' });
+    }
+
+    const transactions = await db.collection('transactions').find({ userId: user._id }).toArray();
+    return res
+      .status(200)
+      .send({ message: 'User transactions succesfully obtained!', transactions });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: 'An error occured when fetching user transactions' });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authenticationRoutes from './routes/authenticationRoutes.js';
+import transactionRoutes from './routes/transactionRoutes.js';
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(authenticationRoutes);
+app.use(transactionRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
