34c5fcc875adddad3985f663e2ed7a187f40de0d
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-08 04:36:24 -0300

    feat: Implemented authorizeUserMiddleware and POST on route "/transactions"

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,27 +1,51 @@
 import db from '../databases/mongodb.js';
+import dayjs from 'dayjs';
+import { transactionSchema } from '../schemas/transactionSchemas.js';
 
 export async function getTransactions(req, res) {
+  // Obtain user
+  const { user } = res.locals;
   try {
-    const { authorization } = req.headers;
-    const token = authorization?.replace('Bearer ', '');
-    if (!token) return res.status(401).send({ message: 'Error: Invalid authentication token!' });
-
-    const session = await db.collection('sessions').findOne({ token });
-    if (!session) {
-      return res.status(401).send({ message: 'Error: Session timeout! Please log in again' });
-    }
-
-    const user = await db.collection('users').findOne({ _id: session.userId });
-    if (!user) {
-      return res.status(404).send({ message: 'Error: User not found!' });
-    }
-
+    // Fetch user transactions from Database
     const transactions = await db.collection('transactions').find({ userId: user._id }).toArray();
     return res
       .status(200)
-      .send({ message: 'User transactions succesfully obtained!', transactions });
+      .send({ message: 'User transactions succesfully obtained', transactions });
+
+    // Error when fetching user transactions
   } catch (error) {
     console.log(error);
     return res.status(500).send({ message: 'An error occured when fetching user transactions' });
+  }
+}
+
+export async function registerTransaction(req, res) {
+  // Obtain transaction
+  const { type, description, value } = req.body;
+
+  // Validate transaction
+  const { error: validError } = transactionSchema.validate({ type, description, value });
+  if (validError) {
+    return res.status(422).send({ message: String(validError) });
+  }
+
+  // Obtain user
+  const { user } = res.locals;
+
+  try {
+    // Insert transaction into Database
+    await db.collection('transactions').insertOne({
+      type,
+      description,
+      value,
+      date: dayjs().format('DD/MM'),
+      userId: user._id,
+    });
+    res.status(200).send({ message: 'Transaction succesfully created' });
+
+    // Error when registering new transaction
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: 'An error occured when registering new transaction' });
   }
 }
--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import { getTransactions } from '../controllers/transactionController.js';
+import { getTransactions, registerTransaction } from '../controllers/transactionController.js';
+import { authorizeUserMiddleware } from '../middleware/authorizeUserMiddleware.js';
 
 const router = Router();
 
-router.get('/transactions', getTransactions);
+router.get('/transactions', authorizeUserMiddleware, getTransactions);
+router.post('/transactions', authorizeUserMiddleware, registerTransaction);
 
 export default router;
