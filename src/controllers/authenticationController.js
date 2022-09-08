81df107c47434d9ab5bd471aff15add042213d7d
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-08 02:33:44 -0300

    feat: Implemented POST on "/sign-up" route

--- src/controllers/authenticationController.js
+++ src/controllers/authenticationController.js
@@ -1,1 +1,29 @@
 import db from '../databases/mongodb.js';
+import joi from 'joi';
+import bcrypt from 'bcrypt';
+
+export async function userSignUp(req, res) {
+  const authSignUpSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().email().required(),
+    password: joi.string().required(),
+    confirmPassword: joi.ref('password'),
+  });
+  try {
+    const user = req.body;
+
+    const { error: validError } = authSignUpSchema.validate(user);
+    console.log(validError);
+    if (validError) return res.status(422).send(String(validError));
+
+    const passwordHash = bcrypt.hashSync(user.password, 10);
+
+    await db
+      .collection('users')
+      .insertOne({ name: user.name, email: user.email, password: passwordHash });
+    res.status(200).send('Succesful sign-up');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('An error occured during user sign-up!');
+  }
+}
--- src/databases/mongodb.js
+++ src/databases/mongodb.js
@@ -5,11 +5,11 @@ dotenv.config();
 
 let db = null;
 
-const mongoClient = new MongoClient(process.env.MONGO_URL);
+const mongoClient = new MongoClient(process.env.MONGO_URI);
 
 try {
   await mongoClient.connect();
-  db = mongoClient.db(process.env.MONGO_DB);
+  db = mongoClient.db(process.env.MONGO_DATABASE_NAME);
   console.log('Database connected successfully!');
 } catch (error) {
   console.error('Error when connecting to Database...');
