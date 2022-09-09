5aacf20260c2ac4b7dd274e7a18e0e0159902518
LucasPCotrim <lucaspcotrim@gmail.com>
2022-09-09 01:38:22 -0300

    fix: Corrected authSignUpSchema

--- src/schemas/authenticationSchemas.js
+++ src/schemas/authenticationSchemas.js
@@ -4,7 +4,7 @@ export const authSignUpSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().required(),
-  confirmPassword: joi.ref('password'),
+  passwordConfirm: joi.any().valid(joi.ref('password')).required(),
 });
 
 export const authLoginSchema = joi.object({
